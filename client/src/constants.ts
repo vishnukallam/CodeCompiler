bf2dc20c71dedca603da8e67622db522593edf68
Vishnu Kallam <127661438+Vishnudluffy@users.noreply.github.com>
2026-04-05 12:06:57 +0530

    Fix deployment node versions for Vercel and Render

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -44,5 +44,5 @@ export const getFileName = (lang: Language): string => {
     }
 };
 
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://codecompiler-cewu.onrender.com';
 
